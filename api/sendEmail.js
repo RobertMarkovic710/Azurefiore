e74301fdd829c4211ea0cf60c91fe01fb9a2e839
Robert Marković <MarkovicR@osijek.hr>
2026-04-28 11:24:50 +0200

    nodemailer install

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -32,6 +32,7 @@ export default async function handler(req, res) {
 
         res.status(200).json({ success: true });
     } catch (error) {
+        console.log(error);
         res.status(500).json({ error: "Greška pri slanju emaila" });
     }
 }
